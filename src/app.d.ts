be966bd938ec295850b231dbc1c44b20ca35e88d
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-02-14 21:07:21 +0100

    docs: add svelte-5-ui-lib version to Footer

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,6 +9,7 @@ declare global {
 		// interface Platform {}
 	}
 	declare const __VERSION__: string;
+	declare const __SVELTE5UILIBVERSION__: string;
 	declare const __SVELTEVERSION__: string;
 	declare const __SVELTEKITVERSION__: string;
 	declare const __VITEVERSION__: string;
--- src/routes/utils/Footer.svelte
+++ src/routes/utils/Footer.svelte
@@ -2,6 +2,7 @@
 	import { Footer, FooterBrand, FooterLi, FooterUl } from 'svelte-5-ui-lib';
 
 	const version = __VERSION__
+	const svelte5uilibVersion = __SVELTE5UILIBVERSION__
 	const svelteVersion = __SVELTEVERSION__
 	const svelteKitVersion = __SVELTEKITVERSION__
 	const viteVersion = __VITEVERSION__
@@ -12,10 +13,13 @@
 	<div class="sm:flex sm:items-center sm:justify-between">
 		<FooterBrand href="/" name="LexiLearn Hub" />
 		<FooterUl
-			ulclass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+			ulclass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 px-4"
 		>
 		<FooterLi href="https://github.com/shinokada/flexilexi" target="_blank"
 				>Flexilexi: {version}</FooterLi
+			>
+			<FooterLi href="https://svelte-5-ui-lib.vercel.app/" target="_blank"
+				>Svelte 5 UI Lib: {svelte5uilibVersion}</FooterLi
 			>
 			<FooterLi href="https://svelte-5-preview.vercel.app/docs/introduction" target="_blank"
 				>Svelte: {svelteVersion}</FooterLi
